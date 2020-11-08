e42652ea0448a4bd0652fb37ff8aaca427b403fe
Christophe <christophe_ngu@outlook.com>
2020-11-08 14:31:07 +0100

    classes User et Administrator added

---
+++ DorsetOOP/Users/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DorsetOOP.Users
+{
+    public abstract class User
+    {
+        public string userId { get; set; }
+        public string Password { get; set; }
+        public bool loginStatus { get; set; }
+
+    }
+}
