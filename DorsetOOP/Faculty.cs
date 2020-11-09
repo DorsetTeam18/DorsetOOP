46bf902c40bfe736d610b116d35d8de2a1d874ec
DorsetTeam18 <72754837+DorsetTeam18@users.noreply.github.com>
2020-11-09 10:40:16 +0100

    Added faculty class (test for Maxime)

---
+++ DorsetOOP/Faculty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DorsetOOP
+{
+    class Faculty
+    {
+        private string nameOfFaculity;
+
+        public string NameOfFacultiy
+        {
+            get { return nameOfFaculity; }
+            set { nameOfFaculity = value; }
+        }
+
+    }
+}
